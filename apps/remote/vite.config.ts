372a483aa08739eec87ed5270e9b19bfdb6015cd
hua yang <yanghua@gzxijiu.cn>
2024-11-25 09:10:57 +0800

    feat: init

---
+++ apps/remote/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { federation } from '@module-federation/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    exclude: ["vue-demi"]
+  },
+  plugins: [
+    vue(),
+    federation({
+      name: 'remote',
+      shared: ["vue-demi"]
+    })
+  ],
+  server: {
+    port: 5001
+  }
+})
